d46928e80551d42b6cf4dce0f54a4963add6ef57
Rafael Batista <rafaelb24@yahoo.com>
2018-11-03 23:00:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,8 +5,13 @@ class Owner
   def self.all
     @@all
   end
+
   def initialize(owner)
     @owner = owner
     self.class.all << self
+  end
+
+  def self.reset_all
+    self.all.reset
   end
 end
