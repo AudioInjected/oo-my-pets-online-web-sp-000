12d4b68af703622ffe5a3b70df779c3287b8204a
Rafael Batista <rafaelb24@yahoo.com>
2018-11-03 22:57:45 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 require 'pry'
 class Owner
-  attr_reader :owner
+  attr_reader :owner, :species
   @@all = []
   def self.all
     @@all
   end
-  def initialize(owner)
+  def initialize(owner, species)
     @owner = owner
+    @species = species
   end
 end
